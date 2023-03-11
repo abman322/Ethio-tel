69619d7a68d26855a6b3f0319511f8b38a86d9dc
Abenezer Yesuf <abman322@gmail.com>
2023-03-11 15:19:26 +0300

    slider

---
+++ components/Slider.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Slider() {
+  return (
+    <div>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQt7XrlOYGCxY4_ESPW5stFkD_pHLunqVVYbQ&usqp=CAU" />
+    </div>
+  );
+}
