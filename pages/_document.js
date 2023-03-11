6777f10b371d23ce2a6c920695bd1bb34c39ed29
Abenezer Yesuf <abman322@gmail.com>
2023-03-11 15:20:09 +0300

    cdn script

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
@@ -6,8 +6,10 @@ export default function Document() {
       <Head />
       <body>
         <Main />
+
         <NextScript />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.4/flowbite.min.js"></script>
       </body>
     </Html>
-  )
+  );
 }
