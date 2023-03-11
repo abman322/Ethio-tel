ce7ee16a569ad429639735d618065c9451a1b825
Abenezer Yesuf <abman322@gmail.com>
2023-03-11 20:31:06 +0300

    test component

---
+++ pages/test.js
@@ -0,0 +1,10 @@
+import ChatBot from "@/components/ChatBot";
+import React, { useState } from "react";
+
+export default function test() {
+  return (
+    <div className=" h-screen">
+      <p>head</p>
+    </div>
+  );
+}
